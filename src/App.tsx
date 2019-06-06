2b7928c5eb779f368cd2e42004e258c0fc63dad8
Cronos <eabz@polispay.org>
2019-06-06 16:01:06 -0500

    Remove navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import MainNavBar from "./components/MainNavBar";
 import MainContent from "./components/MainContent";
 
 const App: React.FC = () => {
   return (
       <div>
-          <MainNavBar/>
           <MainContent/>
       </div>
   );
